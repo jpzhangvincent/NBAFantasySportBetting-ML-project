c28137ca058d4663f655d0a6bbee476df6c627aa
GSR <gsr@DSS04465-SOC-L.local>
2016-03-08 22:31:32 -0800

    added the past salary details

---
+++ Scraping/NumberFire/scrapeSalary.py
@@ -0,0 +1,30 @@
+import re
+from bs4 import BeautifulSoup
+import urllib2
+import pandas
+import csv
+from datetime import date, datetime, timedelta
+
+
+
+def perdelta(start, end, delta):
+    curr = start
+    while curr < end:
+        yield curr
+        curr += delta
+
+
+
+
+for result in perdelta(date(2015, 11, 16), datetime.now().date(), timedelta(days=1)):
+
+    url = 'http://rotoguru1.com/cgi-bin/hyday.pl?game=dk&mon=%s&day=%s&year=%s&scsv=1'%(result.month,result.day,result.year)
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page.read(),"lxml")
+    table = soup.find("pre").getText()
+    # with open("%s.csv"%(result), "w") as text_file:
+    #     text_file.write(table)
+
+
+
+
