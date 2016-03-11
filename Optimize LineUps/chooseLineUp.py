c8a395e41c92eec02c73ce9a79ef735232413af1
RylanSchaeffer <rylanschaeffer@gmail.com>
2016-03-11 12:47:35 -0800

    updated formatting, better commenting

--- Optimize LineUps/chooseLineUp.py
+++ Optimize LineUps/chooseLineUp.py
@@ -1,3 +1,6 @@
+# credit to http://tauserver.wtb.tue.nl/johan/3DprintFactory_ProductionPlanning/knapsack.py
+# for teaching us how to use pulp to solve a knapsack-like problem
+
 import glob
 import pandas
 import pulp
@@ -12,27 +15,27 @@ pastsalaries['DK Salary'] = pastsalaries['DK Salary'].astype(str).map(lambda ele
 pastsalaries.head()
 
 # create dataframe of past winning lineups
-pastwinners = pandas.read_csv('Scraping/NumberFire/numberFireBestTeam.csv')
-# strip '$' from Salary, convert to integer
-pastwinners['Salary'] = pastwinners['Salary'].map(lambda ele: int(ele.replace('$', '')))
+# pastwinners = pandas.read_csv('Scraping/NumberFire/numberFireBestTeam.csv')
+# # strip '$' from Salary, convert to integer
+# pastwinners['Salary'] = pastwinners['Salary'].map(lambda ele: int(ele.replace('$', '')))
 
 
-temp = pastsalaries[pastsalaries['Date'] == 20160306]
-playerNames = temp['Name'].tolist()
-playerCosts = tuple(temp['DK Salary'])
-playerPoints = tuple(temp['DK Pts'])
+specificDate = pastsalaries[pastsalaries['Date'] == 20160306]
 
 
-# calculate optimal lineup
+# initialize variables
+
+playerNames = specificDate['Name'].tolist()
+playerCosts = tuple(specificDate['DK Salary'])
+playerPoints = tuple(specificDate['DK Pts'])
 salaryCap = 55000
 numOfPlayers = range(len(playerCosts))
 
+# create problem
 problem = pulp.LpProblem("Optimal Line-Up", pulp.LpMaximize)
 
-player_vars = pulp.LpVariable.dicts("Players",
-              [i for i in numOfPlayers],
-              0, 1, cat="Binary")
-
+# create variable to represent each player
+player_vars = pulp.LpVariable.dicts("Players", [i for i in numOfPlayers], 0, 1, cat="Binary")
 
 # objective: maximize sum of player points
 problem += pulp.lpSum(player_vars[i] * playerPoints[i] for i in numOfPlayers)
@@ -44,11 +47,8 @@ for i in numOfPlayers:
 # constraints: sum of player costs must be less than or equal to the salary cap
 problem += pulp.lpSum(player_vars[i] * playerCosts[i] for i in numOfPlayers) <= salaryCap
 
-# solve
-solved = problem.solve()
-
-if solved == 1:
-    # print players, whether they are in the solution
+# if solved, print players. otherwise, print error message
+if problem.solve() == 1:
     for pos in range(len(problem.variables())):
         print '%30s, Present = %1.0f' % (playerNames[pos], problem.variables()[pos].varValue)
 else:
