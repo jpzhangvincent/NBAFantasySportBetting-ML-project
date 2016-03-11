6701c6ca4491e28c6799088e95f2e59bead46ec6
RylanSchaeffer <rylanschaeffer@gmail.com>
2016-03-11 12:41:38 -0800

    optimal lineup with real data

--- Optimize LineUps/chooseLineUp.py
+++ Optimize LineUps/chooseLineUp.py
@@ -17,48 +17,36 @@ pastwinners = pandas.read_csv('Scraping/NumberFire/numberFireBestTeam.csv')
 pastwinners['Salary'] = pastwinners['Salary'].map(lambda ele: int(ele.replace('$', '')))
 
 
+temp = pastsalaries[pastsalaries['Date'] == 20160306]
+playerNames = temp['Name'].tolist()
+playerCosts = tuple(temp['DK Salary'])
+playerPoints = tuple(temp['DK Pts'])
+
 
 # calculate optimal lineup
-playerCosts = (5,2,5,5,10,12,14,5,6)
-playerPoints = (9,3,5,3,5,1,9,4,4)
-salaryCap = 10
+salaryCap = 55000
 numOfPlayers = range(len(playerCosts))
 
 problem = pulp.LpProblem("Optimal Line-Up", pulp.LpMaximize)
 
-assign_vars = pulp.LpVariable.dicts("ItemInKnapsack",
+player_vars = pulp.LpVariable.dicts("Players",
               [i for i in numOfPlayers],
               0, 1, cat="Binary")
 
 
 # objective: maximize sum of player points
-problem += pulp.lpSum(assign_vars[i] * playerPoints[i] for i in numOfPlayers)
+problem += pulp.lpSum(player_vars[i] * playerPoints[i] for i in numOfPlayers)
 
 # constraints: each player can only be chosen at most once
 for i in numOfPlayers:
-    problem += pulp.lpSum(assign_vars[i]) <= 1
+    problem += pulp.lpSum(player_vars[i]) <= 1
 
 # constraints: sum of player costs must be less than or equal to the salary cap
-problem += pulp.lpSum(assign_vars[i] * playerCosts[i] for i in numOfPlayers) <= salaryCap
+problem += pulp.lpSum(player_vars[i] * playerCosts[i] for i in numOfPlayers) <= salaryCap
 
-# solve problem with the COIN CBC solver
-#solver = pulp.PULP_CBC_CMD(options=[],keepFiles = 0, msg=1, fracGap=0, maxSeconds = "100")
-#problem.solve(solver)
+# solve
 problem.solve()
 
-# print solution
-print("\n\nThe solution:\n")
 
-print "Knapsack contains items:"
-k=0
-for i in numOfPlayers:
-    if assign_vars[i].varValue > 0:
-        k = k + assign_vars[i].varValue*playerCosts[i]
-        print(i)
-print "with cumulative volume:", round(k, 2), "\n"
-solutionCost = sum(assign_vars[i].varValue * playerPoints[i]  for i in numOfPlayers)
-print "\nThe solution contains:",round(solutionCost),"\n"
-
-
-for v in problem.variables():
-    print v.name, "=", v.varValue
+for pos in range(len(problem.variables())):
+    print '%30s, Present = %1.0f' % (playerNames[pos], problem.variables()[pos].varValue)
